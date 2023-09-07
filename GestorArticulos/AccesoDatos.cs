0b954f15c04fee671d40348b160a78eac5528a82
Nazuti <luquidiba@gmail.com>
2023-09-07 20:01:16 -0300

    Clase Acceso a Datos
    Clase acceso a datos dentro de gestor articulos, para realizar futuras conexiones a base de datos

---
+++ GestorArticulos/AccesoDatos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GestorArticulos
+{
+    class AccesoDatos
+    {
+        private SqlConnection Conexion;
+        private SqlCommand Comando;
+        private SqlDataReader Lector;
+
+        public AccesoDatos()
+        {
+            Conexion = new SqlConnection("");
+            Comando = new SqlCommand();
+        }
+
+        public void SetearQuery(string Query)
+        {
+            Comando.CommandType = System.Data.CommandType.Text;
+            Comando.CommandText = Query;
+        }
+
+        public void CerrarConexion()
+        {
+            if(Lector != null) { 
+                Lector.Close();
+                Conexion.Close();
+            }           
+        }
+
+        public void EjecutarLectura()
+        {
+            Comando.Connection = Conexion;
+            try
+            {
+                Conexion.Open();
+                Lector = Comando.ExecuteReader();
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+    }
+}
