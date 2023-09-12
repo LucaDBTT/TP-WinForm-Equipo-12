ad6cf039e8402526f2e357fdee403babb1ca2c92
Veliz Gerardo <gerardo.veliz@alumnos.frgp.utn.edu.ar>
2023-09-12 18:24:02 -0300

    Front
    Se modifico un poco el form principal, y el form de agregar , se agregaron algunos tooltips tambien

--- GestorArticulos/Agregar.Designer.cs
+++ GestorArticulos/Agregar.Designer.cs
@@ -48,13 +48,15 @@ namespace GestorArticulos
             this.lblPrecio = new System.Windows.Forms.Label();
             this.txtbPrecio = new System.Windows.Forms.TextBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.PanelAgregar = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).BeginInit();
+            this.PanelAgregar.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCodigo
             // 
             this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(61, 95);
+            this.lblCodigo.Location = new System.Drawing.Point(53, 42);
             this.lblCodigo.Name = "lblCodigo";
             this.lblCodigo.Size = new System.Drawing.Size(46, 13);
             this.lblCodigo.TabIndex = 0;
@@ -62,7 +64,7 @@ namespace GestorArticulos
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(96, 510);
+            this.btnAgregar.Location = new System.Drawing.Point(57, 469);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 1;
@@ -71,15 +73,15 @@ namespace GestorArticulos
             // 
             // txtbCodigo
             // 
-            this.txtbCodigo.Location = new System.Drawing.Point(129, 92);
+            this.txtbCodigo.Location = new System.Drawing.Point(117, 35);
             this.txtbCodigo.Name = "txtbCodigo";
-            this.txtbCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtbCodigo.Size = new System.Drawing.Size(111, 20);
             this.txtbCodigo.TabIndex = 2;
             this.toolTip1.SetToolTip(this.txtbCodigo, "Ingrese el codigo.. ");
             // 
             // pboxImagen
             // 
-            this.pboxImagen.Location = new System.Drawing.Point(322, 106);
+            this.pboxImagen.Location = new System.Drawing.Point(343, 40);
             this.pboxImagen.Name = "pboxImagen";
             this.pboxImagen.Size = new System.Drawing.Size(328, 359);
             this.pboxImagen.TabIndex = 3;
@@ -88,14 +90,15 @@ namespace GestorArticulos
             // cboxMarca
             // 
             this.cboxMarca.FormattingEnabled = true;
-            this.cboxMarca.Location = new System.Drawing.Point(119, 302);
+            this.cboxMarca.Location = new System.Drawing.Point(117, 245);
             this.cboxMarca.Name = "cboxMarca";
-            this.cboxMarca.Size = new System.Drawing.Size(121, 21);
+            this.cboxMarca.Size = new System.Drawing.Size(111, 21);
             this.cboxMarca.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.cboxMarca, "Seleccione una marca..");
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(227, 510);
+            this.btnCancelar.Location = new System.Drawing.Point(188, 469);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 5;
@@ -106,7 +109,7 @@ namespace GestorArticulos
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(61, 143);
+            this.lblNombre.Location = new System.Drawing.Point(53, 90);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(50, 13);
             this.lblNombre.TabIndex = 6;
@@ -115,7 +118,7 @@ namespace GestorArticulos
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(61, 197);
+            this.lblDescripcion.Location = new System.Drawing.Point(30, 140);
             this.lblDescripcion.Name = "lblDescripcion";
             this.lblDescripcion.Size = new System.Drawing.Size(69, 13);
             this.lblDescripcion.TabIndex = 7;
@@ -124,7 +127,7 @@ namespace GestorArticulos
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(61, 254);
+            this.label4.Location = new System.Drawing.Point(35, 197);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 8;
@@ -133,7 +136,7 @@ namespace GestorArticulos
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(61, 305);
+            this.lblMarca.Location = new System.Drawing.Point(56, 253);
             this.lblMarca.Name = "lblMarca";
             this.lblMarca.Size = new System.Drawing.Size(43, 13);
             this.lblMarca.TabIndex = 9;
@@ -141,38 +144,41 @@ namespace GestorArticulos
             // 
             // txtbNombre
             // 
-            this.txtbNombre.Location = new System.Drawing.Point(129, 140);
+            this.txtbNombre.Location = new System.Drawing.Point(117, 83);
             this.txtbNombre.Name = "txtbNombre";
-            this.txtbNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtbNombre.Size = new System.Drawing.Size(111, 20);
             this.txtbNombre.TabIndex = 10;
             this.toolTip1.SetToolTip(this.txtbNombre, "Ingrese el nombre del articulo..");
             // 
             // txtbDescripcion
             // 
-            this.txtbDescripcion.Location = new System.Drawing.Point(129, 190);
+            this.txtbDescripcion.Location = new System.Drawing.Point(117, 133);
             this.txtbDescripcion.Name = "txtbDescripcion";
-            this.txtbDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtbDescripcion.Size = new System.Drawing.Size(111, 20);
             this.txtbDescripcion.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.txtbDescripcion, "Ingrese una breve descripcion..");
             // 
             // txtbUrlImagen
             // 
-            this.txtbUrlImagen.Location = new System.Drawing.Point(129, 247);
+            this.txtbUrlImagen.Location = new System.Drawing.Point(117, 190);
             this.txtbUrlImagen.Name = "txtbUrlImagen";
-            this.txtbUrlImagen.Size = new System.Drawing.Size(100, 20);
+            this.txtbUrlImagen.Size = new System.Drawing.Size(111, 20);
             this.txtbUrlImagen.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.txtbUrlImagen, "Ingrese la url...");
             // 
             // cboxCategoria
             // 
             this.cboxCategoria.FormattingEnabled = true;
-            this.cboxCategoria.Location = new System.Drawing.Point(119, 354);
+            this.cboxCategoria.Location = new System.Drawing.Point(117, 297);
             this.cboxCategoria.Name = "cboxCategoria";
-            this.cboxCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cboxCategoria.Size = new System.Drawing.Size(111, 21);
             this.cboxCategoria.TabIndex = 13;
+            this.toolTip1.SetToolTip(this.cboxCategoria, "Seleccione una categoria...");
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Location = new System.Drawing.Point(61, 354);
+            this.lblCategoria.Location = new System.Drawing.Point(45, 305);
             this.lblCategoria.Name = "lblCategoria";
             this.lblCategoria.Size = new System.Drawing.Size(58, 13);
             this.lblCategoria.TabIndex = 14;
@@ -181,7 +187,7 @@ namespace GestorArticulos
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(61, 401);
+            this.lblPrecio.Location = new System.Drawing.Point(53, 351);
             this.lblPrecio.Name = "lblPrecio";
             this.lblPrecio.Size = new System.Drawing.Size(43, 13);
             this.lblPrecio.TabIndex = 15;
@@ -189,38 +195,49 @@ namespace GestorArticulos
             // 
             // txtbPrecio
             // 
-            this.txtbPrecio.Location = new System.Drawing.Point(119, 401);
+            this.txtbPrecio.Location = new System.Drawing.Point(117, 344);
             this.txtbPrecio.Name = "txtbPrecio";
-            this.txtbPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtbPrecio.Size = new System.Drawing.Size(111, 20);
             this.txtbPrecio.TabIndex = 16;
+            this.toolTip1.SetToolTip(this.txtbPrecio, "Ingrese el precio...");
+            // 
+            // PanelAgregar
+            // 
+            this.PanelAgregar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PanelAgregar.Controls.Add(this.txtbNombre);
+            this.PanelAgregar.Controls.Add(this.txtbPrecio);
+            this.PanelAgregar.Controls.Add(this.lblCodigo);
+            this.PanelAgregar.Controls.Add(this.lblPrecio);
+            this.PanelAgregar.Controls.Add(this.txtbCodigo);
+            this.PanelAgregar.Controls.Add(this.lblCategoria);
+            this.PanelAgregar.Controls.Add(this.cboxMarca);
+            this.PanelAgregar.Controls.Add(this.cboxCategoria);
+            this.PanelAgregar.Controls.Add(this.lblNombre);
+            this.PanelAgregar.Controls.Add(this.txtbUrlImagen);
+            this.PanelAgregar.Controls.Add(this.lblDescripcion);
+            this.PanelAgregar.Controls.Add(this.txtbDescripcion);
+            this.PanelAgregar.Controls.Add(this.label4);
+            this.PanelAgregar.Controls.Add(this.lblMarca);
+            this.PanelAgregar.Location = new System.Drawing.Point(33, 40);
+            this.PanelAgregar.Name = "PanelAgregar";
+            this.PanelAgregar.Size = new System.Drawing.Size(285, 412);
+            this.PanelAgregar.TabIndex = 17;
             // 
             // frmAgregar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(683, 613);
-            this.Controls.Add(this.txtbPrecio);
-            this.Controls.Add(this.lblPrecio);
-            this.Controls.Add(this.lblCategoria);
-            this.Controls.Add(this.cboxCategoria);
-            this.Controls.Add(this.txtbUrlImagen);
-            this.Controls.Add(this.txtbDescripcion);
-            this.Controls.Add(this.txtbNombre);
-            this.Controls.Add(this.lblMarca);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.lblDescripcion);
-            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.PanelAgregar);
             this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.cboxMarca);
             this.Controls.Add(this.pboxImagen);
-            this.Controls.Add(this.txtbCodigo);
             this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.lblCodigo);
             this.Name = "frmAgregar";
             this.Text = "Agregar";
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).EndInit();
+            this.PanelAgregar.ResumeLayout(false);
+            this.PanelAgregar.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -244,5 +261,6 @@ namespace GestorArticulos
         private System.Windows.Forms.Label lblCategoria;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.TextBox txtbPrecio;
+        private System.Windows.Forms.Panel PanelAgregar;
     }
 }
--- GestorArticulos/Catalogo.Designer.cs
+++ GestorArticulos/Catalogo.Designer.cs
@@ -30,7 +30,6 @@ namespace GestorArticulos
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.asd = new System.Windows.Forms.ColorDialog();
             this.dgvArticulo = new System.Windows.Forms.DataGridView();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.txtboxBuscar = new System.Windows.Forms.TextBox();
@@ -40,26 +39,33 @@ namespace GestorArticulos
             this.btnELiminar = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.pboxImagen = new System.Windows.Forms.PictureBox();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFiltroEspecifico = new System.Windows.Forms.Label();
+            this.cboxCampo = new System.Windows.Forms.ComboBox();
+            this.cboxCriterio = new System.Windows.Forms.ComboBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvArticulo
             // 
             this.dgvArticulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvArticulo.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dgvArticulo.Location = new System.Drawing.Point(133, 144);
+            this.dgvArticulo.Location = new System.Drawing.Point(199, 63);
             this.dgvArticulo.MultiSelect = false;
             this.dgvArticulo.Name = "dgvArticulo";
             this.dgvArticulo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvArticulo.Size = new System.Drawing.Size(456, 238);
+            this.dgvArticulo.Size = new System.Drawing.Size(552, 318);
             this.dgvArticulo.TabIndex = 0;
             this.dgvArticulo.SelectionChanged += new System.EventHandler(this.dgvArticulo_SelectionChanged);
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(348, 26);
+            this.lblTitulo.Location = new System.Drawing.Point(196, 22);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(118, 13);
             this.lblTitulo.TabIndex = 1;
@@ -67,15 +73,15 @@ namespace GestorArticulos
             // 
             // txtboxBuscar
             // 
-            this.txtboxBuscar.Location = new System.Drawing.Point(182, 118);
+            this.txtboxBuscar.Location = new System.Drawing.Point(389, 19);
             this.txtboxBuscar.Name = "txtboxBuscar";
-            this.txtboxBuscar.Size = new System.Drawing.Size(407, 20);
+            this.txtboxBuscar.Size = new System.Drawing.Size(362, 20);
             this.txtboxBuscar.TabIndex = 2;
             this.toolTip1.SetToolTip(this.txtboxBuscar, "Busque un articulo...");
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(196, 388);
+            this.btnAgregar.Location = new System.Drawing.Point(230, 398);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 3;
@@ -86,7 +92,7 @@ namespace GestorArticulos
             // lblBuscar
             // 
             this.lblBuscar.AutoSize = true;
-            this.lblBuscar.Location = new System.Drawing.Point(130, 118);
+            this.lblBuscar.Location = new System.Drawing.Point(337, 19);
             this.lblBuscar.Name = "lblBuscar";
             this.lblBuscar.Size = new System.Drawing.Size(46, 13);
             this.lblBuscar.TabIndex = 4;
@@ -94,7 +100,7 @@ namespace GestorArticulos
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(321, 388);
+            this.btnModificar.Location = new System.Drawing.Point(413, 398);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(75, 23);
             this.btnModificar.TabIndex = 6;
@@ -103,7 +109,7 @@ namespace GestorArticulos
             // 
             // btnELiminar
             // 
-            this.btnELiminar.Location = new System.Drawing.Point(452, 388);
+            this.btnELiminar.Location = new System.Drawing.Point(600, 398);
             this.btnELiminar.Name = "btnELiminar";
             this.btnELiminar.Size = new System.Drawing.Size(75, 23);
             this.btnELiminar.TabIndex = 7;
@@ -113,19 +119,75 @@ namespace GestorArticulos
             // 
             // pboxImagen
             // 
-            this.pboxImagen.Location = new System.Drawing.Point(614, 144);
+            this.pboxImagen.Location = new System.Drawing.Point(784, 63);
             this.pboxImagen.Name = "pboxImagen";
-            this.pboxImagen.Size = new System.Drawing.Size(310, 277);
+            this.pboxImagen.Size = new System.Drawing.Size(406, 318);
             this.pboxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pboxImagen.TabIndex = 5;
             this.pboxImagen.TabStop = false;
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(24, 252);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(121, 20);
+            this.txtFiltro.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.txtFiltro, "Busque un articulo...");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.btnFiltrar);
+            this.panel1.Controls.Add(this.cboxCriterio);
+            this.panel1.Controls.Add(this.txtFiltro);
+            this.panel1.Controls.Add(this.cboxCampo);
+            this.panel1.Controls.Add(this.lblFiltroEspecifico);
+            this.panel1.Location = new System.Drawing.Point(2, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 526);
+            this.panel1.TabIndex = 9;
+            // 
+            // lblFiltroEspecifico
+            // 
+            this.lblFiltroEspecifico.AutoSize = true;
+            this.lblFiltroEspecifico.Location = new System.Drawing.Point(61, 26);
+            this.lblFiltroEspecifico.Name = "lblFiltroEspecifico";
+            this.lblFiltroEspecifico.Size = new System.Drawing.Size(35, 13);
+            this.lblFiltroEspecifico.TabIndex = 5;
+            this.lblFiltroEspecifico.Text = "Filtrar ";
+            // 
+            // cboxCampo
+            // 
+            this.cboxCampo.FormattingEnabled = true;
+            this.cboxCampo.Location = new System.Drawing.Point(24, 86);
+            this.cboxCampo.Name = "cboxCampo";
+            this.cboxCampo.Size = new System.Drawing.Size(121, 21);
+            this.cboxCampo.TabIndex = 6;
+            // 
+            // cboxCriterio
+            // 
+            this.cboxCriterio.FormattingEnabled = true;
+            this.cboxCriterio.Location = new System.Drawing.Point(24, 175);
+            this.cboxCriterio.Name = "cboxCriterio";
+            this.cboxCriterio.Size = new System.Drawing.Size(121, 21);
+            this.cboxCriterio.TabIndex = 7;
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(49, 327);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrar.TabIndex = 10;
+            this.btnFiltrar.Text = "Buscar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
             // 
             // frmCatalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.ClientSize = new System.Drawing.Size(972, 543);
+            this.ClientSize = new System.Drawing.Size(1247, 591);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnELiminar);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.pboxImagen);
@@ -140,14 +202,14 @@ namespace GestorArticulos
             this.Load += new System.EventHandler(this.frmCatalogo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ColorDialog asd;
         private System.Windows.Forms.DataGridView dgvArticulo;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.TextBox txtboxBuscar;
@@ -157,6 +219,12 @@ namespace GestorArticulos
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnELiminar;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.ComboBox cboxCriterio;
+        private System.Windows.Forms.ComboBox cboxCampo;
+        private System.Windows.Forms.Label lblFiltroEspecifico;
     }
 }
 
